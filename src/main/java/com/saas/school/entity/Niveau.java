5c50f100b16ea86b10d84a09a8f37b5b5daed1b9
Fousseny TRAORE <foussenytraore692@gmail.com>
2026-09-25 11:19:50 +0000

    Bulletin primaire

--- src/main/java/com/saas/school/entity/Niveau.java
+++ src/main/java/com/saas/school/entity/Niveau.java
@@ -17,7 +17,7 @@ public class Niveau {
     @ManyToOne
     @JoinColumn(name = "ecole_id")
     private Ecole ecole;
-    private  int NoteMax;
+
     @ManyToOne
     @JoinColumn(name = "cycle_id")
     private Cycle cycle; // optionnel, pour ne pas casser les niveaux existants sans cycle
